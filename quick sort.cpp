a84909a9c8b30788038967e4ad1a41a5f7676e55
DEY, ARPITA <136344442+ArpitaDey0826@users.noreply.github.com>
2024-06-11 21:48:31 +0600

    Add files via upload

---
+++ quick sort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+}
+
+void printArray(const vector<int>& arr) {
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> numbers = {5, 2, 9, 1, 5, 6};
+
+    cout << "Original array: ";
+    printArray(numbers);
+
+    int n = numbers.size();
+    quickSort(numbers, 0, n - 1);
+
+    cout << "Sorted array: ";
+    printArray(numbers);
+
+    return 0;
+}
